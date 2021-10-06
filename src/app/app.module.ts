abe9e68b647737d5852eef14a4e730b5340d3b09
lion <breakermoob@hotmail.com>
2021-10-06 02:46:10 -0500

    web component full

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { DoBootstrap, Injector, NgModule } from '@angular/core';
+import { ScrollUpButtonComponent } from './scroll-up-button/scroll-up-button.component';
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   declarations: [
-    AppComponent
+    ScrollUpButtonComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [ScrollUpButtonComponent]
 })
-export class AppModule { }
+export class AppModule implements DoBootstrap {
+  constructor(
+    private injector: Injector
+  ) {
+    const imagenEspacio = createCustomElement(ScrollUpButtonComponent, { injector });
+    customElements.define('arq-mf-scroll-up-button', imagenEspacio);
+  }
+  ngDoBootstrap() { }
+}
